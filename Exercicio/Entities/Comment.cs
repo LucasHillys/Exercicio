80290408e123aca2e9e198b77062e8774928b4ac
Lucas Hillys <lucashillysz@gmail.com>
2024-06-18 14:46:48 -0400

    first commit

---
+++ Exercicio/Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercicio.Entities
+{
+    internal class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
---
+++ Exercicio/Entities/Post.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exercicio.Entities
+{
+    internal class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
